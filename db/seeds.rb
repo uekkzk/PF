d9034b759225ab26af2d36d1be5d6a93f19573da
vagrant <vagrant@localhost.localdomain>
2020-10-26 06:46:08 +0000

    commit5

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,16 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+
+User.create!(
+  [
+    {name: "ぴょんす", email: "pyonsu@gmail.com", password: "pyonsu"},
+    {name: "ぴょんす2", email: "pyonsu2@gmail.com", password: "pyonsu2"}
+  ]
+)
+
+Parking.create!(
+  [
+    {user_id: 1, parking_name: "ぴょんすパーキング", prefecture: 27, address: "通天閣", regular_holiday: "年中無休", fee: "3時間100円"}
+  ]
+)
